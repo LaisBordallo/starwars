10501bf3d6bbf5308327cdc447511d67c9e47f8f
Lais <laisbordallo@outlook.com>
2024-08-20 11:16:21 -0300

    Cria HTML e CSS para a navbar, footer, home e container principal. Tem adição de mídia ao background.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "./layout/footer/footer.component";
+import { NavbarComponent } from "./layout/navbar/navbar.component";
+import { HomeComponent } from "./layout/home/home.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FooterComponent, NavbarComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    './src/**/*.{html,ts,js,scss}',    
+    "./node_modules/tw-elements/js/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
